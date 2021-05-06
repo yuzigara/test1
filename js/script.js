4e6ae7cd8c6aa7b119bd3de78919b311528631db
Han <yuzigara@gmail.com>
2021-05-06 20:42:12 +0300

    taze barlag

--- js/script.js
+++ js/script.js
@@ -1,20 +1,58 @@
-const numberofFilms = +prompt('Nache film gorduniz', '');
+const adamsany = +prompt('Nache talyp bar', '');
 
-const personalMovieDB = {
-    count: numberofFilms,
-    movies: {},
-    actors: {},
-    genres: {},
-    privat: false
-};
+const topar = {
+    talypsan: adamsany,
+    okde: {},
+    yalta: {},
+}
 
-const a = prompt('In sonky goren kinon', ''),
-      b = prompt('Nahili haladynmy shony', ''),
-      c = prompt('In sonky goren kinon', ''),
-      d = prompt('Nahili haladynmy shony', '');
+const a = prompt('Okde talybyn ady', ''),
+      b = prompt('Okde talybyn yetishigi', ''),
+      c = prompt('Yaltanyn ady', ''),
+      d = prompt('Yaltanyn yetishigi', '');
+
+topar.okde[a] = b,
+topar.yalta[c] = d;
+
+console.log(topar)
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const numberofFilms = +prompt('Nache film gorduniz', '');
+
+// const personalMovieDB = {
+//     count: numberofFilms,
+//     movies: {},
+//     actors: {},
+//     genres: {},
+//     privat: false
+// };
+
+// const a = prompt('In sonky goren kinon', ''),
+//       b = prompt('Nahili haladynmy shony', ''),
+//       c = prompt('In sonky goren kinon', ''),
+//       d = prompt('Nahili haladynmy shony', '');
+
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
