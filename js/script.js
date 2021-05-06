996ac4a8a6167057539d4f8ec6ba875909ec2cf9
yuzigara <yuzigara@gmail.com>
2021-05-06 11:13:06 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use sctrict";
+
+// alert('Hello')
+
+// const result = confirm("Siz ylalashyanyzmy?");
+// console.log(result);
+
+// const answer = +prompt("Sizin yashynyz nache?", "16");
+// console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt("Adynyz name?");
+answers[1] = prompt("Familyanyz name?");
+answers[2] = prompt("Yashynyz nache?");
+
+document.write(answers);
