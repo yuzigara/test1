b568d8b5faa9f7564229ca337e0cd21dd7fa0abe
Han <yuzigara@gmail.com>
2021-05-07 09:22:41 +0300

    siklin 4 versiony

--- sikl.js
+++ sikl.js
@@ -14,7 +14,10 @@ let num = 50;
 
 while (num <= 55)
 
-for (let i = 1; i < 8; i++) {
-    console.log(num);
-    num++;
+for (let i = 1; i < 10; i++) {
+    if (i ===6) {
+        continue;
+    }
+    console.log(i);
+    
 }
