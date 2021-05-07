79e03779c4e421d7fb6fe8a81478a14aa110c623
Han <yuzigara@gmail.com>
2021-05-07 06:53:05 +0300

    novyy script

--- js/script.js
+++ js/script.js
@@ -1,22 +1,60 @@
-const mashgala = +prompt('Mashgalada nache adam bar?', '');
+"use strict"
 
-const rayatlar = {
-    mashgalasany: mashgala, 
-    ejesi: {},
-    kakasy: {},
-    dogany: {},
-    jigisi: {}
-};
-
-const a = prompt("ejesinin ady", ''),
-      b = prompt("kakasynyn ady", ''),
-      c = prompt("doganyn ady", ''),
-      d = prompt("jigisinin ady", '');
+if (4 == 9) {
+    console.log('Ok');
+} else {
+    console.log('Error');
+}
 
 
-rayatlar.ejesi = a,
-rayatlar.kakasy = b,
-rayatlar.dogany = c,
-rayatlar.jigisi = d,
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Many');
+// } else {
+//     console.log('Ok!');
+// }
+// (num == 50) ? console.log('Ok!') : console.log('Error!');
 
-console.log(rayatlar);
+
+const num = 54;
+
+switch (num) {
+    case 49: 
+        console.log('Nadogry');
+        break;
+    case 100: 
+        console.log('Nadogry');
+        break;
+    case 50:
+        console.log('Dogry');
+        break;
+    default:
+        console.log('Tazeden synlashyn');
+        break; 
+}
+
+
+
+// const mashgala = +prompt('Mashgalada nache adam bar?', '');
+
+// const rayatlar = {
+//     mashgalasany: mashgala, 
+//     ejesi: {},
+//     kakasy: {},
+//     dogany: {},
+//     jigisi: {}
+// };
+
+// const a = prompt("ejesinin ady", ''),
+//       b = prompt("kakasynyn ady", ''),
+//       c = prompt("doganyn ady", ''),
+//       d = prompt("jigisinin ady", '');
+
+
+// rayatlar.ejesi = a,
+// rayatlar.kakasy = b,
+// rayatlar.dogany = c,
+// rayatlar.jigisi = d,
+
+// console.log(rayatlar);
