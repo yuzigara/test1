a01809f839f205d252b9178ea15388871da0ba8d
Han <yuzigara@gmail.com>
2021-05-07 09:59:42 +0300

    db commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const numberOfFilms = +prompt('How many time did you watch films?');
+
+const personalMovieDB = {
+    count: {},
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: {}
+};
+
+const a = prompt('What film did you watch?', '');
+const b = prompt('How much score you give?', '');
+const c = prompt('What film did you watch?', '');
+const d = prompt('How much score you give?', '');
+
+personalMovieDB.count = numberOfFilms,
+personalMovieDB.movies[a] = b,
+personalMovieDB.movies[c] = d;
