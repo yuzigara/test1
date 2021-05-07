1b00e2cca8c61bc00b9f927cc87c0dea3eabf52d
Han <yuzigara@gmail.com>
2021-05-07 09:11:54 +0300

    uchunji sikl

--- sikl.js
+++ sikl.js
@@ -7,9 +7,14 @@ let num = 50;
 //     num++;
 // }
 
-do {
+// do {
+//     console.log(num);
+//     num++;
+// }
+
+while (num <= 55)
+
+for (let i = 1; i < 8; i++) {
     console.log(num);
     num++;
 }
-
-while (num <= 55)
